87ca54a2d0e28627e32a3ee8bd81119450fd75c5
Marcus <marcusoliveirapc@gmail.com>
2019-08-14 11:40:54 -0300

    Leitura e escrita ok
    Geocode ok
    Ler csv ok
    escrever csv ok

--- Geoprocessamento.py
+++ Geoprocessamento.py
@@ -1,34 +1,37 @@
+# coding=utf-8
+#Aplicação Python de geocodificação em batch de endereços;
+#Este script usa um serviços de geoficador de endereços do ArcGIS e Komoot.
+#O projeto precisa um arquivo de entrada csv de modo que funcione corretamente.
+#Sua principal funcionalidade é promover um banco de dados de todas as localidades do Brasil com Latitude e longitude
 
-#Marcus Oliveira 
+#Marcus Oliveira 01-08-201
 
-
-
+import time
+import geocoder
 import pandas as pd
-
-
-
-# ----------------------------- Variaveis ------------------------------------#
-
-
-
+import requests
 
 # ----------------------------- Entrada de dados -----------------------------#
 
 
 input_file_path = "input.csv"
- 
+output_file_path = "output"
 
 
 Nome_coluna_endereco = "ENDERECO"
 Nome_coluna_cidade = "CIDADE"
-Nome_coluna_cep = "CEP"   
+Nome_coluna_cep = "CEP"
 
+
+indice = 0
+quantidade = 100
+escrita_quantidade = 1000
+geocode = 3
+espera = 3
 
 # ----------------------------- Leitura do csv -----------------------------#
 
-#leitura
 arquivo = pd.read_csv(input_file_path, low_memory=False)
-
 
 if Nome_coluna_endereco not in arquivo.columns:
     raise ValueError("Não foi possivel localizar a coluna do endereço.")
@@ -41,3 +44,48 @@ if (Nome_coluna_cep):
     Enderecos = (arquivo[Nome_coluna_endereco] + ', ' + arquivo[Nome_coluna_cep].astype(str) + ', ' + arquivo[Nome_coluna_cidade]).tolist()
 else:
     Enderecos = (arquivo[Nome_coluna_endereco] + ', ' + arquivo[Nome_coluna_cidade]).tolist()
+
+
+# ----------------------------- Função -----------------------------#
+
+
+class GeoSessao:
+    def __init__(self):
+        self.Arcgis = requests.Session()
+        self.Komoot = requests.Session()
+
+
+def criar_sessao():
+    return GeoSessao()
+
+def geocode_endereco(endereco, s):
+    g = geocoder.arcgis(endereco, session=s.Arcgis)
+    if (g.ok == False):
+        g = geocoder.komoot(endereco, session=s.Komoot)
+
+    return g
+
+
+def encontrar_endereco(endereco, s, tentativa, espera):
+    g = geocode_endereco(endereco, s)
+    if (g.ok == False):
+        time.sleep(espera)
+        s = criar_sessao()
+        if (tentativa > 0):
+            encontrar_endereco(endereco, s, tentativa-1, espera+espera)
+    return g
+
+
+# ----- out csv --- #
+
+
+
+def Arquivo_saida(data, index):
+    Nome_criar_sessao = (output_file_path + str(index) + ".csv")
+    print("Criado com sucesso o arquivo de saida " + Nome_criar_sessao)
+    done = pd.DataFrame(data)
+    done.columns = ['Endereco', 'Lat', 'Long', 'Provedor']
+    done.to_csv((Nome_criar_sessao + ".csv"), sep=',', encoding='utf8')
+
+
+print("Processo terminado com sucesso !")
