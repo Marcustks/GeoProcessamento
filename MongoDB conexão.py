9af44e790ea38baf8dc2631967732746ecdbee3f
Marcus Oliveira <marcusoliveirapc@gmail.com>
2019-08-20 14:13:00 -0300

    conexão ok

--- MongoDB conexão.py
+++ MongoDB conexão.py
@@ -1,9 +1,13 @@
 import pymongo
-from pymongo import MongoClient
+from pprint import pprint
 
 
-client: MongoClient = pymongo.MongoClient("mongodb+srv://sys:<marcossales21>@cluster-tcc-z3g4b.mongodb.net/test?retryWrites=true&w=majority")
-db = client['tcc']
-album = db['txt']
+client = pymongo.MongoClient(
+    "mongodb+srv://sys:marcossales21@cluster-tcc-z3g4b.mongodb.net/test?retryWrites=true&w=majority")
+db = client.tcc
+collection = db.txt
 
-album.find_one()
+
+cursor = collection.find({})
+for document in cursor:
+    pprint(document)
