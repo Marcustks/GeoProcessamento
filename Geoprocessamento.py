3434ced6dcac560257b12e6796e3c0b1730633dc
Marcus Oliveira <marcusoliveirapc@gmail.com>
2019-08-22 11:23:07 -0300

    Mongodb ok.

--- Geoprocessamento.py
+++ Geoprocessamento.py
@@ -9,6 +9,7 @@
 import time
 import geocoder
 import pandas as pd
+import pymongo
 import requests
 
 
@@ -18,54 +19,57 @@ import requests
 # ----------------------------- Entrada de dados -----------------------------#
 
 
+client = pymongo.MongoClient(
+    "mongodb+srv://sys:marcossales21@cluster-tcc-z3g4b.mongodb.net/test?retryWrites=true&w=majority")
+db = client.tcc
+collection = db.txt
+print('conexão ok')
 
 
 
-input_file_path = "input.csv"
-output_file_path = "output"
+x= '1'
+while True:
+ x = str(x)
+ endereco = collection.find_one( {"ID": x })
+ aux_pais = 'Brasil'
+ aux_endereco = endereco['ENDEREÇO']
+ aux_municipio = endereco['CIDADE']
+ aux_cep = endereco['CEP']
+ aux_bairro = endereco['BAIRRO']
+ print(aux_cep)
+ print(aux_municipio)
+ print(aux_endereco)
+ print(aux_bairro)
+
+ endereco_geral = (str(aux_pais) +","+ str(aux_cep)+","+str(aux_municipio)+","+str(aux_endereco))
+ print(endereco_geral)
+ print("--------")
 
 
-Nome_coluna_endereco = "ENDERECO"
-Nome_coluna_cidade = "CIDADE"
-Nome_coluna_cep = "CEP"
+ indice = 0
+ quantidade = 10000
+ escrita_quantidade = 100000
+ geocode = 3
+ espera = 3
 
 
-indice = 0
-quantidade = 10000
-escrita_quantidade = 100000
-geocode = 3
-espera = 3
-
-# ----------------------------- Leitura do csv -----------------------------#
-
-arquivo = pd.read_csv(input_file_path, low_memory=False, error_bad_lines=False)
-
-if Nome_coluna_endereco not in arquivo.columns:
-    raise ValueError("Não foi possivel localizar a coluna do endereço.")
-if Nome_coluna_cidade not in arquivo.columns:
-    raise ValueError("Não foi possivel localizar a coluna da cidade.")
-
-if (Nome_coluna_cep):
-    if Nome_coluna_cep not in arquivo.columns:
-        raise ValueError("Não foi possivel localizar a coluna de cep.")
-    Enderecos = (arquivo[Nome_coluna_endereco] + ', ' + arquivo[Nome_coluna_cep].astype(str) + ', ' + arquivo[Nome_coluna_cidade]).tolist()
-else:
-    Enderecos = (arquivo[Nome_coluna_endereco] + ', ' + arquivo[Nome_coluna_cidade]).tolist()
-
+ Resultado = []
+ falhas = 0
+ total_falhas = 0
 
 # ----------------------------- Função -----------------------------#
 
 
-class GeoSessao:
+ class GeoSessao:
     def __init__(self):
         self.Arcgis = requests.Session()
         self.Komoot = requests.Session()
 
 
-def criar_sessao():
+ def criar_sessao():
     return GeoSessao()
 
-def geocode_endereco(endereco, s):
+ def geocode_endereco(endereco, s):
     g = geocoder.arcgis(endereco, session=s.Arcgis)
     if (g.ok == False):
         g = geocoder.komoot(endereco, session=s.Komoot)
@@ -73,81 +77,45 @@ def geocode_endereco(endereco, s):
     return g
 
 
-def encontrar_endereco(endereco, s, tentativa, espera):
+ def encontrar_endereco(endereco, s, tentativa, espera):
     g = geocode_endereco(endereco, s)
     if (g.ok == False):
         time.sleep(espera)
         s = criar_sessao()
         if (tentativa > 0):
-            encontrar_endereco(endereco, s, tentativa-1, espera+espera)
+            encontrar_endereco(endereco_geral, s, tentativa-1, espera+espera)
     return g
 
 
 # ----- saida csv --- #
+ s = criar_sessao()
+ g = encontrar_endereco(endereco_geral, s, geocode, espera)
+ print("oque quero")
+ print(g)
+ print(g.latlng,g.provider)
+ aux_lat = g.lat
+ aux_lng = g.lng
 
 
 
-def Arquivo_saida(data, index):
-    Nome_arquivo_saida = (output_file_path + str(index) + ".csv")
-    print("Criado com sucesso o arquivo de saida" + Nome_arquivo_saida)
-    done = pd.DataFrame(data)
-    done.columns = ['endereco', 'Lat', 'Long', 'Provedor']
-    done.to_csv((Nome_arquivo_saida + ".csv"), sep=',', encoding='utf8')
+ geoprocessamento_banco =db.Geoprocessamento
 
-#----------- var02 -------------#
-s = criar_sessao()
-Resultado = []
-falhas = 0
-total_falhas = 0
-Progesso = len(Enderecos) - indice
+ Geoprocessamento_insert = {
+               "_id": x ,
+              "Pais": "Brasil",
+              "Municipio": aux_municipio ,
+              "bairro": aux_bairro ,
+              "Endereço": aux_endereco,
+              "Cep": aux_cep,
+              "Latitude ": aux_lat,
+              "longitude": aux_lng,
+               "Array_lat_long": g.latlng
+             }
+ print("insert")
+ geoprocessamento_banco.insert_one(Geoprocessamento_insert).inserted_id
+ x = int(x)
+ x=x+1
 
-# ----------------------------- busca -----------------------------#
+ if x == '10':
+  break
 
-for i, endereco in enumerate(Enderecos[indice:]):
-
-    if ((indice + i) % quantidade == 0):
-        total_falhas += falhas
-        print(
-            "Completado {} de {}. falhas {} para {} no total.".format(i + indice, Progesso, falhas, total_falhas))
-        falhas = 0
-
-
-    try:
-        g = encontrar_endereco(endereco, s, geocode, espera)
-        if (g.ok == False):
-            Resultado.append([endereco, "Não", "Foi", "Possivel encontrar o endereço"])
-            print("sucesso ao endereço: " + endereco)
-            falhas += 1
-        else:
-            Resultado.append([endereco, g.latlng[0], g.latlng[1], g.provider])
-
-
-    except Exception as e:
-        print("Foi encontrada {} Falhas para o endereco {}. Tentar novamente....".format(e, endereco))
-        try:
-            time.sleep(5)
-            s = criar_sessao()
-            g = geocode_endereco(endereco, s)
-            if (g.ok == False):
-                print("Não foi possivel encontrar.")
-                Resultado.append([endereco, "Não", "foi", "Possivel encontrar o endereço"])
-                falhas += 1
-            else:
-                print("Encontrado")
-                Resultado.append([endereco, g.latlng[0], g.latlng[1], g.provider])
-        except Exception as e:
-            print("Foi encontrada {} falhas para o endereco {} again.".format(e, endereco))
-            falhas += 1
-            Resultado.append([endereco, e, e, "ERROR"])
-
-
-    if (i%escrita_quantidade == 0 and i != 0):
-        Arquivo_saida(Resultado, i + indice)
-
-    print(i, g.latlng, g.provider)
-
-
-
-
-Arquivo_saida(Resultado, i + indice + 1)
-print("Fim da execução!:")
--- MongoDB conexão.py
+++ MongoDB conexão.py
@@ -6,8 +6,21 @@ client = pymongo.MongoClient(
     "mongodb+srv://sys:marcossales21@cluster-tcc-z3g4b.mongodb.net/test?retryWrites=true&w=majority")
 db = client.tcc
 collection = db.txt
+print('conexão ok')
 
+x= '1'
+endereco = collection.find_one( {"ID": x })
 
-cursor = collection.find({})
-for document in cursor:
-    pprint(document)
+aux_pais = 'Brasil'
+aux_endereco = endereco['ENDEREÇO']
+aux_municipio = endereco['CIDADE']
+aux_cep = endereco['CEP']
+aux_bairro = endereco['BAIRRO']
+print(aux_cep)
+print(aux_municipio)
+print(aux_endereco)
+print(aux_bairro)
+
+endereco_geral = (str(aux_pais) +","+ str(aux_cep))
+print(endereco_geral)
+
